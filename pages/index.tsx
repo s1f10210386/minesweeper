96af5f166a9b3b1c9129be42e62ca1e9be70f9d4
s1f10210386 <s1f102103864@iniad.org>
2021-12-17 04:34:41 +0900

    枠

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   height: 100vh;
-  font-size: 0;
+  font-size: 0; /* inline-blockの隙間消せる */
   background-color: #c0c0c0;
 `
 const Board = styled.div`
@@ -43,6 +43,8 @@ const Block = styled.div`
   width: 25px;
   height: 25px;
   background-color: #808080;
+  border: 1px solid;
+  border-bottom: transparent; /* 下が透明になる */
 `
 
 const Home: NextPage = () => {
